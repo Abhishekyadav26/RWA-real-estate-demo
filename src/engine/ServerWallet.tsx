0d1d75402b2f8d27fd020fefbab66896a6f3c496
Abhishekyadav26 <aky732244@gmail.com>
2025-07-04 00:42:28 +0530

    code

--- src/engine/ServerWallet.tsx
+++ src/engine/ServerWallet.tsx
@@ -3,6 +3,6 @@ import { client } from "../client";
 
 export const serverWallet = Engine.serverWallet({
     client: client,
-    address: import.meta.env.SERVER_WALLET_ADDRESS,
-    vaultAccessToken: import.meta.env.VAULT_ACCESS_TOKEN,
+    address: "0x046704eC64349B42Ef0f49379c97Cb56248e46ab" as string,
+    vaultAccessToken: "vt_act_GQQ6RGGKRTBSRZB3FXFXTYA7HKB7LWLGS5TWGE6XBPCO7FJFYTEMJG7XNF2YERZXUUEFGNLXUZ5KQAH5CBOCV36QIJIIEGQSGFVLKDCZ" as string,
 });
